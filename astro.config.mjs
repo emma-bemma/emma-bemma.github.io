bcff4aa5811b23a5c4ef7e5d59e8d73f59e399a8
EyePulp <eyepulp@eyepulp.net>
2023-12-23 14:04:25 -0600

    fix site name config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  })]
+    site: 'https://emma-bemma.github.io',
+    integrations: [
+        tailwind({
+            applyBaseStyles: false,
+        }),
+    ],
 });
